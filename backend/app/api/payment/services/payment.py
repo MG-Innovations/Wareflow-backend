51d86386ec1f248756ba663e06264955440e6bb4
hiteshshimpi <shimpihitesh2@gmail.com>
2024-06-29 02:26:19 +0530

    Resoveld deves branch conflict

--- backend/app/api/payment/services/payment.py
+++ backend/app/api/payment/services/payment.py
@@ -2,16 +2,6 @@ from uuid import UUID
 from sqlalchemy.orm import Session
 from typing import List, Optional
 from app.api.payment.db_models.payment import Payment
-<<<<<<< HEAD
-from app.api.orders.services.order import order as order_service
-from app.core.enums import PaymentStatus
-from app.api.payment.schemas.payment import Payment as BaseProduct , PaymentDelete , PaymentGet , PaymentGetResponse , PaymentUpdate
-
-class PaymentService:
-    
-    def create_payment(self, db: Session, payment: BaseProduct, user_id: UUID, tenant_id: UUID) -> Payment:
-        db_payment = Payment(**payment.model_dump(), tenant_id=tenant_id, user_id=user_id, created_by=user_id, updated_by=user_id)
-=======
 from app.api.payment.schemas.payment import (
     Payment as BaseProduct,
     PaymentDelete,
@@ -33,7 +23,6 @@ class PaymentService:
             created_by=user_id,
             updated_by=user_id
         )
->>>>>>> feat-trans-deves
         db.add(db_payment)
         
         # Retrieve and update the order's amount_received and status
--- backend/app/api/product/services/product.py
+++ backend/app/api/product/services/product.py
@@ -24,17 +24,10 @@ class ProductService:
         db.commit()
         db.refresh(db_product)
         return db_product
-<<<<<<< HEAD
     
     def get_products(self, db: Session,tenant_id:UUID,skip:int=0,limit:int=10) -> List[Product]:
         return db.query(Product).filter_by(tenant_id=tenant_id).offset(skip).limit(limit).all()
     
-=======
-
-    def get_products(self, db: Session) -> List[Product]:
-        return db.query(Product).all()
-
->>>>>>> feat-trans-deves
     def get_product_by_id(self, db: Session, product_id: UUID) -> Optional[Product]:
         return db.query(Product).filter(Product.id == product_id).first()
 
@@ -46,46 +39,4 @@ class ProductService:
             return product_id
         return None
 
-<<<<<<< HEAD
 product_service = ProductService()
-=======
-    # def get_company(self, db: Session, company_id: UUID) -> Optional[Company]:
-    #     return db.query(Company).filter(Company.id == company_id).first()
-
-    # def create_product_type(self, db: Session, product_type: ProductTypeSchema, user_id: UUID) -> ProductType:
-    #     db_product_type = ProductType(**product_type.dict(), created_by=user_id, updated_by=user_id)
-    #     db.add(db_product_type)
-    #     db.commit()
-    #     db.refresh(db_product_type)
-    #     return db_product_type
-
-    # def get_product_types(self, db: Session) -> List[ProductType]:
-    #     return db.query(ProductType).all()
-
-    # def get_product_type(self, db: Session, product_type_id: UUID) -> Optional[ProductType]:
-    #     return db.query(ProductType).filter(ProductType.id == product_type_id).first()
-
-    # def create_product(self, db: Session, product: ProductSchema, user_id: UUID) -> Product:
-    #     db_product = Product(**product.dict(), created_by=user_id, updated_by=user_id)
-    #     db.add(db_product)
-    #     db.commit()
-    #     db.refresh(db_product)
-    #     return db_product
-
-    # def get_products(self, db: Session) -> List[Product]:
-    #     return db.query(Product).all()
-
-    # def get_product(self, db: Session, product_id: UUID) -> Optional[Product]:
-    #     return db.query(Product).filter(Product.id == product_id).first()
-
-    # def delete_product(self, db: Session, product_id: UUID) -> Optional[UUID]:
-    #     product = db.query(Product).filter(Product.id == product_id).first()
-    #     if product:
-    #         db.delete(product)
-    #         db.commit()
-    #         return product_id
-    #     return None
-
-
-product_service = ProductService()
->>>>>>> feat-trans-deves
